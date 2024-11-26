0c10e5d453ffee64a98c0942fc651195fa64c9fc
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2024-11-26 05:27:50 +0300

    инит файл с нужными импортами для внешних модулей

---
+++ domconnect_selenium/__init__.py
@@ -0,0 +1,3 @@
+from domconnect_selenium.xpaths import xpaths
+from domconnect_selenium.helpers import (validate_date_time, validate_ip_port,
+                                         second_captcha_routine, first_captcha_routine)
